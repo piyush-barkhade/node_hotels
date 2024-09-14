5222440fb5338d1c24b3313125b1cbd46d169ed2
piyush-barkhade <piyushbarkhade.pb@gmail.com>
2024-09-15 01:12:28 +0530

    test

--- db.js
+++ db.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb://localhost:27017/hotels";
-
-mongoose.connect(mongoURL);
+//const mongoURL = "mongodb://localhost:27017/hotels";
 
 const db = mongoose.connection;
 
-db.on('connected', ()=>{
-  console.log("Connected")
-}
-)
+db.on("connected", () => {
+  console.log("Connected");
+});
 
-db.on('error', (e)=>{
-  console.error("Connected", e)
-}
-)
+db.on("error", (e) => {
+  console.error("Connected", e);
+});
 
-db.on('disconnected', ()=>{
-  console.log("Disconnected")
-}
-)
+db.on("disconnected", () => {
+  console.log("Disconnected");
+});
 
 module.exports = db;
